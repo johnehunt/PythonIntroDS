89ccfa0059c8419fedb27c09151febd296b0645c
John Hunt <john.hunt10@gmail.com>
2021-05-25 11:22:07 +0100

    Added module sample code

---
+++ 13-modules/main3.py
@@ -0,0 +1,11 @@
+from utils import *
+
+
+def main():
+	printer(default_shape)
+	shape = Shape("circle")
+	printer(shape)
+
+
+if __name__ == "__main__":
+	main()
---
+++ 13-modules/utils.py
@@ -0,0 +1,25 @@
+"""This is a test module"""
+
+print('Hello I am the utils module')
+
+
+def printer(some_object):
+    print('printer')
+    print(some_object)
+    print('done')
+
+
+class Shape:
+
+    def __init__(self, id):
+        self.id = id
+
+    def __str__(self):
+        return 'Shape - ' + self.id
+
+
+default_shape = Shape('square')
+
+
+def _special_function():
+    print('Special function')
