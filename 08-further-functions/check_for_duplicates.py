6dcbdbf38f1f2834840ad9577e70a00896a68d79
John Hunt <john.hunt10@gmail.com>
2021-06-15 14:49:48 +0100

    Examples of how to find duplicates in a list

---
+++ 08-further-functions/check_for_duplicates.py
@@ -0,0 +1,10 @@
+data_list = [1, 1, 1, 2, 3, 2, 3, 4, 6, 8, 1, 0]
+
+# Get a count of each duplicate value
+dupes = filter(lambda i: data_list.count(i) > 1, data_list)
+dupe_count = set([(i, data_list.count(i)) for i in dupes])
+print(dupe_count)
+
+# Just the values that have duplicates
+duplicated_values = set(filter(lambda i: data_list.count(i) > 1, data_list))
+print(duplicated_values)
