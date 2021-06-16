46683d3fb04635deb7aa7cb67606dd4bd5e4662d
John Hunt <john.hunt10@gmail.com>
2021-06-16 12:00:17 +0100

    Extended example

--- 08-further-functions/check_for_duplicates.py
+++ 08-further-functions/check_for_duplicates.py
@@ -1,5 +1,9 @@
 data_list = [1, 1, 1, 2, 3, 2, 3, 4, 6, 8, 1, 0]
 
+# Get a list of all duplicated values
+duplicates = list(filter(lambda i: data_list.count(i) > 1, data_list))
+print(duplicates)
+
 # Just the values that have duplicates
 duplicated_values = set(filter(lambda i: data_list.count(i) > 1, data_list))
 print(duplicated_values)
@@ -9,14 +13,3 @@ print(duplicated_values)
 dupes = filter(lambda i: data_list.count(i) > 1, data_list)
 dupe_count = set((i, data_list.count(i)) for i in dupes)
 print(dupe_count)
-
-
-# Long hand form using a dictionary
-duplicates = {}  # Set up empty dictionary
-dupes = filter(lambda i: data_list.count(i) > 1, data_list)
-for item in dupes:
-    count = duplicates.get(item)
-    count = count if count is not None else 0
-    duplicates[item] = count + 1
-
-print(duplicates)
