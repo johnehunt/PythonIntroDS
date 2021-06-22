3a6cf12d5e80b83321eaad783f102acb2c10d772
John Hunt <john.hunt10@gmail.com>
2021-06-22 16:19:08 +0100

    Changed print to show element access within row

--- 16-files/read_csv_file.py
+++ 16-files/read_csv_file.py
@@ -4,6 +4,6 @@ print('Starting to read csv file')
 with open('sample.csv') as csvfile:
 	reader = csv.reader(csvfile)
 	for row in reader:
-		print(row)
+		print(f'{row[0]}, {row[1]}')
 
 print('Done Reading')
