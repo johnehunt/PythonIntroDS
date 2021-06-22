ca2c872872524888f66d8d561e890f5b9bd83f54
John Hunt <john.hunt10@gmail.com>
2021-06-22 14:48:43 +0100

    Added a assertpy examnple

---
+++ 15-testing/assertpy-example-test.py
@@ -0,0 +1,8 @@
+""" Illustrates using assertpy style assertions """
+from assertpy import assert_that
+
+
+def test_something():
+    assert_that(1 + 2).is_equal_to(3)
+    assert_that('foobar').is_length(6).starts_with('foo').ends_with('bar')
+    assert_that(['a', 'b', 'c']).contains('a').does_not_contain('x')
