95f28c70fedb4519dd17ebebbd60f37a8ddd824c
John Hunt <john.hunt10@gmail.com>
2023-03-02 14:19:43 +0000

    Minor update

--- 14-files/read_csv_file.py
+++ 14-files/read_csv_file.py
@@ -1,9 +1,12 @@
 import csv
 
-print('Starting to read csv file')
-with open('sample.csv') as csvfile:
-	reader = csv.reader(csvfile)
-	for row in reader:
-		print(f'{row[0]}, {row[1]}')
+try:
+	print('Starting to read csv file')
+	with open('sample.csv') as csvfile:
+		reader = csv.reader(csvfile)
+		for row in reader:
+			print(f'{row[0]}, {row[1]}')
+except Exception as exp:
+	print(exp)
 
 print('Done Reading')
