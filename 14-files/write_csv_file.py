7b2ef5196bacf971e0cfe0895a0a115972e3c96a
John Hunt <john.hunt10@gmail.com>
2023-03-02 14:20:36 +0000

    Minor update

--- 14-files/write_csv_file.py
+++ 14-files/write_csv_file.py
@@ -1,9 +1,12 @@
 import csv
 
-print('Creating CSV file')
-with open('sample.csv', 'w') as csvfile:
-	writer = csv.writer(csvfile)
-	writer.writerow(['She Loves You', 'Sept 1963'])
-	writer.writerow(['I Want to Hold Your Hand', 'Dec 1963'])
-	writer.writerow(['Cant Buy Me Love', 'Apr 1964'])
-	writer.writerow(['A Hard Days Night', 'July 1964'])
+try:
+	print('Creating CSV file')
+	with open('sample.csv', 'w') as csvfile:
+		writer = csv.writer(csvfile)
+		writer.writerow(['She Loves You', 'Sept 1963'])
+		writer.writerow(['I Want to Hold Your Hand', 'Dec 1963'])
+		writer.writerow(['Cant Buy Me Love', 'Apr 1964'])
+		writer.writerow(['A Hard Days Night', 'July 1964'])
+except Exception as exp:
+	print(exp)
