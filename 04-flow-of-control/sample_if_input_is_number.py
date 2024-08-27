b529f12a36d04270314d5e3923fcd7ac4a982663
John Hunt <john.hunt10@gmail.com>
2024-08-27 12:48:50 +0100

    Updates

--- 04-flow-of-control/sample_if_input_is_number.py
+++ 04-flow-of-control/sample_if_input_is_number.py
@@ -1,4 +1,7 @@
+# Illustrates a simple if statement
+# Note checks to see if the string that has been input only contains number characters
 input_string = input('Please input a number: ')
+
 if input_string.isnumeric():
     print('The number is accepted')
 else:
--- 04-flow-of-control/whilesample.py
+++ 04-flow-of-control/whilesample.py
@@ -1,10 +1,14 @@
 # Illustrates basic while loop
 
-count = 0
-print('Starting')
-while count < 10:
-    print(count, '', end='')
-    count += 1
+valid_data = False
+
+while not valid_data:
+    age = int(input('Please enter your age: '))
+    if age < 0 or age > 120:
+        print('invalid Age')
+    else:
+        valid_data = True
 
 print()  # not part of the while loop
+
 print('Done')
